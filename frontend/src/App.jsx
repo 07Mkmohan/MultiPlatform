216be3a274164180dfb9f2ea558c0fc56093ca58
07Mkmohan <chhaparimk162511@gmail.com>
2025-09-09 22:17:39 +0530

    initial deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/pages/PreHome";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import UserHomePage from "./components/pages/User/UserHomePage";
-import AdminHome from "./components/pages/AdminHome.jsx/AdminHome";
+import AdminHome from "./components/pages/AdminHome/AdminHome";
 import RecruterHome from "./components/pages/Recruter/RecruterHome";
 import ProfilePage from "./components/pages/User/UserProfile";
 
